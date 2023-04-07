658ce90a431c7db5a4b45c73e4cd0741b9288233
NickdevC <nick_coles@msn.com>
2023-04-07 14:00:30 +0000

    Add spread operator to ensure useState values remain rendered

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import ControlledForm from './components/ControlledForm';
 import ControlledFormHooks from './components/ControlledFormHooks';
 import UseStateWithArrays from './components/UseStateWithArrays';
+import UseStateWithObjects from './components/UseStateWithObjects';
 
 function App() {
   return (
     <div className="App">
       <UseStateWithArrays />
+      <UseStateWithObjects />
     </div>
   );
 }
