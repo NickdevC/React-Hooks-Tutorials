6c506912fdb50c676c36a2edb9169dba44d71783
NickdevC <nick_coles@msn.com>
2023-04-07 13:32:39 +0000

    Add addNums function

--- my-app/src/components/UseStateWithArrays.js
+++ my-app/src/components/UseStateWithArrays.js
@@ -1,9 +1,18 @@
 import React, {useState} from 'react'
 
 function UseStateWithArrays() {
+    const [nums, setNums] = useState([1,2,3]);
+    const addNums = () => {
+        setNums([...nums, nums.length + 1]);
+    }
   return (
     <div>
-        It works!
+        <button onClick={addNums}>
+            Add Item
+        </button>
+        <ul>
+            {nums.map(num => <li key={num}>{num}</li>)}
+        </ul>
     </div>
   )
 }
